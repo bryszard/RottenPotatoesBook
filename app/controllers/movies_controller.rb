77a7a9681ff2a34a4c488226d00f29fd33b66750
bryszard1 <bryszard@poczta.onet.pl>
2016-01-18 21:52:30 +0000

    Third commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,46 @@
 class MoviesController < ApplicationController
     
     def index
-        @movies = Movie.all
+        @movies = Movie.all.sort_by { |m| m.title }
     end
 
     def show
+        begin
         id = params[:id]
         @movie = Movie.find(id)
+        rescue ActiveRecord::RecordNotFound
+            redirect_to movies_path
+        end
     end
     
+    def new
+        # render a form - 'new' template
+    end
+    
+    def create
+        @movie = Movie.create!(params[:movie])
+        flash[:notice] = "#{@movie.title} has been successfully created."
+        redirect_to movie_path(@movie)
+    end
+    
+    def edit
+        @movie = Movie.find params[:id]
+    end
+    
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(params[:movie])
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        respond_to do |client_wants|
+            client_wants.html { redirect_to movie_path(@movie) }
+            client_wants.xml { render :xml => @movie.to_xml }
+        end
+    end
+    
+    def destroy
+        @movie = Movie.find params[:id]
+        @movie.destroy
+        flash[:notice] = "#{@movie.title} was successfully deleted."
+        redirect_to movies_path
+    end
 end
