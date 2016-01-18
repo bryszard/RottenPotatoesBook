b64356bdf69ef24f36f4a2f177466e3adfcc566b
bryszard1 <bryszard@poczta.onet.pl>
2016-01-18 16:10:55 +0000

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
-Rails.application.routes.draw do
+Myrottenpotatoes::Application.routes.draw do
+  
+  resources :movies
+  root :to => redirect('/movies')
+
+
+
+#Rails.application.routes.draw do
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
